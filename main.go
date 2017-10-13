799906739a962e9cd01c7ea326f3ec4c9c437f2e
Matthew Dempsky <mdempsky@google.com>
2017-10-13 15:40:08 -0700

    rangerdanger: initial import
    Tool detects range statements iterating over addressable arrays.

---
+++ main.go
@@ -0,0 +1,57 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go/ast"
+	"go/token"
+	"go/types"
+	"log"
+
+	"github.com/kisielk/gotool"
+	"golang.org/x/tools/go/loader"
+)
+
+var fset = token.NewFileSet()
+
+func main() {
+	flag.Parse()
+
+	var conf loader.Config
+	conf.Fset = fset
+	for _, importPath := range gotool.ImportPaths(flag.Args()) {
+		conf.Import(importPath)
+	}
+	prog, err := conf.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, pkg := range prog.InitialPackages() {
+		for _, file := range pkg.Files {
+			ast.Inspect(file, func(node ast.Node) bool {
+				if s, ok := node.(*ast.RangeStmt); ok {
+					danger(s, pkg)
+				}
+				return true
+			})
+		}
+	}
+}
+
+func danger(s *ast.RangeStmt, pkg *loader.PackageInfo) {
+	if s.Value == nil {
+		return
+	}
+
+	tv := pkg.Types[s.X]
+	if _, ok := tv.Type.Underlying().(*types.Array); !ok || !tv.Addressable() {
+		return
+	}
+
+	fmt.Printf("%s: range over addressable array\n", fset.Position(s.Pos()))
+}
